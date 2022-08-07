3f897cd3d3868c706bce55ac196bab2f863a268b
Piyush-M01 <modipiyush2001@gmail.com>
2022-08-07 22:42:17 +0530

    first try

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "your-email@domain.com"n
+echo "\
+[general]\n\
+email = \"piyush.modi@learner.manipal.edu\"\n\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = 8501
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
