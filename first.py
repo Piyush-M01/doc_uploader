e7bf79af920b524c56f3a711178e88dfd8603780
Piyush-M01 <modipiyush2001@gmail.com>
2022-08-09 00:55:30 +0530

    Modified

--- first.py
+++ first.py
@@ -1,13 +1,27 @@
+import base64
 import streamlit as st
 import pandas as pd
 import docx2txt
 import pdfplumber
 from summa import keywords
+from PIL import Image
+from fpdf import FPDF
 
 class doc_uploader:
 
     def __init__(self):
         st.title("Document Uploader")
+        st.markdown(""" <style>#MainMenu {visibility: hidden;}footer {visibility: hidden;}</style> """, unsafe_allow_html=True)
+        self.reduce_padding()
+    
+    def reduce_padding(self):
+        padding = 0
+        st.markdown(f""" <style>.reportview-container .main .block-container{{
+                padding-top: {padding}rem;
+                padding-right: {padding}rem;
+                padding-left: {padding}rem;
+                padding-bottom: {padding}rem;
+            }} </style> """, unsafe_allow_html=True)
 
     # @st.cache(suppress_st_warning=True)
     def uploader(self):
@@ -45,14 +59,35 @@ class doc_uploader:
             st.write(value)
             self.submitted = st.form_submit_button("Submit")
         if self.submitted==True:
-            self.modified_data=str(docx2txt.process(self.docx_file))+"\n \n"+value
-            st.write(self.modified_data)
+            try:
+                self.modified_data=str(docx2txt.process(self.docx_file))+"\n \n"+value
+                st.write(self.modified_data)
+            except:
+                st.write(value)
+                self.modified_data=value
             self.keyword_extractor(self.modified_data)
             
 
+    # def create_download_link(self,data,filename):
+    #     pdf = FPDF()
+    #     pdf.add_page()
+    #     f = open("text_plain", "r")
+    #     pdf.set_font("Arial", size = 15)
+    #     for x in f:
+    #         pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
+    #     pdf.output(filename)
+    #     with open(filename, 'rb') as h_pdf:
+    #         st.download_button(
+    #             label="Download PDF",
+    #             data=h_pdf,
+    #             file_name=filename,
+    #             mime="application/pdf",
+    #         )
+
     def doc_downloader(self,data):
         st.subheader("Download the text file")
-        st.download_button('Download Text File',data, 'text/plain')
+        st.download_button('Download Text File',data, 'text/plain.docx')
+        # self.create_download_link(data,"text_pdf.pdf")
 
     def keyword_extractor(self,data):
         self.TR_keywords = keywords.keywords(data, scores=True)
@@ -67,6 +102,15 @@ class doc_uploader:
         self.doc_downloader(data)
 
 if __name__=="__main__":
+    img=Image.open('upload.png')
+    st.set_page_config(page_title="Doc Uploader",page_icon=img)
+    hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
     x=doc_uploader()
     x.uploader()
     x.make_form()
